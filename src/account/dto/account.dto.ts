d9af83b47af984bbbd9d2b720b5978a8743db0ce
braqt <brian.quinteros216@gmail.com>
2024-02-20 21:49:04 -0300

    add Decorators for validations on the account dto

--- src/account/dto/account.dto.ts
+++ src/account/dto/account.dto.ts
@@ -1,8 +1,22 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export default class AccountDto {
+  @IsNotEmpty()
   firebaseAuthID: string;
+
+  @IsNotEmpty()
   firstName: string;
+
+  @IsNotEmpty()
   lastName: string;
+
+  @IsNotEmpty()
   phone: string;
+
+  @IsNotEmpty()
   documentNumber: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
 }
--- src/account/dto/getAccounts.dto.ts
+++ src/account/dto/getAccounts.dto.ts
@@ -1,9 +1,16 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class GetAccountsDto {
+  @IsNotEmpty()
   pageNumber: number;
+
+  @IsNotEmpty()
   pageSize: number;
+
   name: string;
 }
 
 export class GetAccountDto {
+  @IsNotEmpty()
   id: number;
 }
